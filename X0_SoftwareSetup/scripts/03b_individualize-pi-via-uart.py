8ed62dbb588c54a432d1bf22c98fe37224a4e8e3
Adam Kewley <contact@adamkewley.com>
2026-04-09 11:33:56 +0200

    Add saving device hostname/password to the picredentials CSV on-update

--- X0_SoftwareSetup/scripts/03b_individualize-pi-via-uart.py
+++ X0_SoftwareSetup/scripts/03b_individualize-pi-via-uart.py
@@ -3,7 +3,9 @@
 import argparse
 import csv
 import serial
+import os
 from pathlib import Path
+import tempfile
 
 base_username = "pbl"           # All devices have the same username (doesn't change)
 base_hostname = "raspberrypi"   # All not-individualized devices have this hostname
@@ -44,6 +46,56 @@ class DeviceDB:
                 return device["password"]
         return default
 
+    # Sets the hostname for the device identified by `mac_address` to `new_hostname`
+    # or creates a new device entry with the given `mac_address` and `new_hostname`.
+    def set_hostname_for_mac_address(self, mac_address, new_hostname):
+        for device in self.devices:
+            if device["mac"] == mac_address:
+                device["hostname"] = new_hostname
+                return
+        # Else, not found: create a new one
+        new_device = {"hostname": new_hostname, "mac": mac_address, "password": None}
+        self.devices.append(new_device)
+
+    # Sets the password for the device identified by `mac_address` to `new_password`
+    # or raises a `RuntimeError` if a device entry with `mac_address` does not exist
+    # (always call `set_hostname_for_mac_address` first).
+    def set_password_for_mac_address(self, mac_address, new_password):
+        for device in self.devices:
+            if device["mac"] == mac_address:
+                device["password"] = new_password
+                return
+        raise RuntimeError(f"Device entry for {mac_address} not found, create one with an appropriate hostname first")
+
+    # Overwrites the loaded device list CSV with the device list stored by
+    # this `DeviceDB`.
+    def save(self):
+        # Write the new content to a temporary file next to
+        # the input file and then atomically move it over
+        # the input file to minimize the chance of half-writes
+        # or data destruction.
+        tmpfile = None
+        try:
+            tmpfile = tempfile.NamedTemporaryFile(
+                mode='wt',
+                dir=self.db_path.parent,
+                prefix=self.db_path.name,
+                delete=False
+            )
+            headers = ["hostname", "mac", "password"]
+            writer = csv.DictWriter(tmpfile, fieldnames=headers)
+            writer.writeheader()
+            writer.writerows(self.devices)
+
+            # Writing complete, flush it and atomically move it
+            # over the old file.
+            tmpfile.close()
+            Path(tmpfile.name).replace(self.db_path)  # Atomic move
+            tmpfile = None                            # Disable cleanup (moved)
+        finally:
+            if tmpfile:
+                os.remove(tmpfile.name)
+
 def do_individualization_via_serial(device_db_path, serial_port):
     # Load a db
     db = DeviceDB(device_db_path)
@@ -52,6 +104,7 @@ def do_individualization_via_serial(device_db_path, serial_port):
         # Send ENTER (newline), which makes the Pi respond with its login prompt
         # (if it hasn't already).
         ser.write(b"\n")
+        ser.flush()
 
         # Read line-by-line until a login prompt is detected
         login_data = ser.read_until(b"login: ")
@@ -122,7 +175,8 @@ def do_individualization_via_serial(device_db_path, serial_port):
                     save_db = True
 
                 if save_db:
-                    pass  # TODO: write updated CSV
+                    print("Saving updated credentials database")
+                    db.save()
 
                 print(f"Running individualization with the following parameters:")
                 print(f"- hostname = {new_hostname} (old = {hostname})")
@@ -148,7 +202,7 @@ def main():
     parser.add_argument("device_csv", help="Path to CSV file containing Pi credentials")
     parser.add_argument("serial_port", help="Path to the serial port the Pi is connected to (e.g. /dev/ttyUSB0)")
     args = parser.parse_args()
-    do_individualization_via_serial(args.device_csv, args.serial_port)
+    do_individualization_via_serial(Path(args.device_csv), args.serial_port)
 
 if __name__ == "__main__":
     main()
