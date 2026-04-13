71c27b281d5c52af77f0da1ae49e42ec09800554
Adam Kewley <contact@adamkewley.com>
2026-04-13 14:22:54 +0200

    Pin pip dependency versions

--- X0_SoftwareSetup/scripts/setup_pi/05_install-pip-dependencies.sh
+++ X0_SoftwareSetup/scripts/setup_pi/05_install-pip-dependencies.sh
@@ -3,12 +3,12 @@
 set -xeuo pipefail
 
 required_pip_dependencies=(
-    "Adafruit-Blinka"                 # S4:      CircuitPython support
-    "adafruit-circuitpython-ads1x15"  # S4:      library for the ADS1115 board
-    "guizero"                         # L3 & S1: used to build GUIs in the lecture
-    "matplotlib"                      # Common:  suggested in L2 and used by many lectures/practicals
-    "spidev"                          # S2:      dependency from SenseHAT (https://www.waveshare.com/wiki/Sense_HAT_(B))
-    "svgwrite"                        # S1:      transitive dependency for project-posenet
+    "adafruit-blinka==8.64.0"                # S4:      CircuitPython support
+    "adafruit-circuitpython-ads1x15==3.0.3"  # S4:      library for the ADS1115 board
+    "guizero==1.1.1"                         # L3 & S1: used to build GUIs in the lecture
+    "matplotlib==3.3.4"                      # Common:  suggested in L2 and used by many lectures/practicals
+    "spidev==3.5"                            # S2:      dependency from SenseHAT (https://www.waveshare.com/wiki/Sense_HAT_(B))
+    "svgwrite==1.4.3"                        # S1:      transitive dependency for project-posenet
 )
 
 echo "----- starting install pip dependencies -----"
