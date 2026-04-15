511f659dcbcc140aabcdcc8f26da315dc26f410a
Adam Kewley <contact@adamkewley.com>
2026-04-15 09:22:59 +0200

    Add flash-compressed-pi-image.sh

---
+++ X0_SoftwareSetup/scripts/flash-compressed-pi-image.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+usage="usage: $0 SOURCE_IMG_XZ DESTINATION_BLOCK_DEVICE"
+
+if [[ $# -ne 2 ]]; then
+    echo "${usage}" 1>&2
+    exit 1
+fi
+
+image_file=$1
+block_device=$2
+
+# Ensure the chosen block device is a USB device, just to reduce the
+# chance of accidently flashing the caller's root filesystem or home
+# drive (it can happen!)
+if ! udevadm info --query property --name "${block_device}" | grep -q '^ID_BUS=usb$'; then
+    echo "${block_device}: does not appear to be a USB device - are you SURE it's the microSD card (remove this check if you're sure)"
+    exit 1
+fi
+
+xz -dc "${image_file}" | sudo dd of=${block_device} iflag=fullblock oflag=dsync bs=512K status=progress
+sync
